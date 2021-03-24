3cad01a65ca6d8ecbf66120adac7ea57a70d02f1
Josh Gray <gray.josh@gmail.com>
2021-03-24 09:26:43 +0100

    test changes

---
+++ test_code/arrays/failing/int64_array.c
@@ -0,0 +1,6 @@
+int main()
+{
+	long long vals[10];
+	vals[5] = 10;
+	return (int)vals[5];
+}
--- test_code/arrays/int_array.c
+++ test_code/arrays/int_array.c
@@ -1,7 +1,7 @@
 int main()
 {
 	int vals[10];
-//	int x = 5;
-	vals[1] = 7;
-	return vals[1];
+	int x = 5;
+	vals[5] = x;
+	return vals[5];
 }
